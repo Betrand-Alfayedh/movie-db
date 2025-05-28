087724860835ec4f3695691d4697fa1f66612472
Betrand-Alfayedh <btrnd.alfydh@gmail.com>
2025-05-28 13:57:20 +0700

    login, logout, validasi

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Movie extends Model
 {
@@ -14,5 +15,16 @@ class Movie extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    protected $fillable = ['title', 'slug', 'synopsis', 'category_id', 'year', 'actors', 'cover_image'];
+
+    protected $guarded = [];
+
+    public function getCoverImageUrlAttribute()
+    {
+        return Str::startsWith($this->cover_image, 'http')
+            ? $this->cover_image
+            : asset('images/' . $this->cover_image);
     }
 }
